feaeeb4cacddc866871463cb0217ed88ba2af57d
Farzaneh-Mohammadi <mohammadifh8931@gmail.com>
2022-12-09 08:59:39 +0330

    add todoList component

---
+++ components/TodoContent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import AddTaskButton from './AddTaskButton'
+import TodoModal from './TodoModal'
+import TodoList from './TodoList'
+
+const TodoContent = () => {
+  return (
+    <div>
+        <AddTaskButton />
+<TodoModal />
+<TodoList />
+    </div>
+  )
+}
+
+export default TodoContent
